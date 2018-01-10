05ee5805704025b24e3ff9c8f9817cb366c21831
manoj-anvaya <manoj@anvayaanalytics.com>
2018-01-10 20:29:02 +0530

    Commit 69:
    Gradient texts added need to be fixed.

--- app/src/main/java/com/hustler/quote/ui/textFeatures/TextFeatures.java
+++ app/src/main/java/com/hustler/quote/ui/textFeatures/TextFeatures.java
@@ -533,7 +533,7 @@ public class TextFeatures {
                     LinearGradient lin_grad = new LinearGradient(0, 0, 0, 50, colors, position, Shader.TileMode.MIRROR);
                     shader_gradient[0] = lin_grad;
                     gradientPreviewText.getPaint().setShader(null);
-                    Bitmap bitmap= BitmapFactory.decodeResource(editorActivity.getResources(),AnimUtils.createDrawable(firstColor[0], secondColor[0], editorActivity))
+                    Bitmap bitmap= BitmapFactory.decodeResource(editorActivity.getResources(),AnimUtils.createDrawable(firstColor[0], secondColor[0], editorActivity));
 
                     ((TextView)dialog.findViewById(R.id.gradient_preview_text)).getPaint().setShader(shader_gradient[0]);
                     gradientText.setBackground(AnimUtils.createDrawable(firstColor[0], secondColor[0], editorActivity));
