fcff378af86b939219832a422488808ee396b539
Sayi5047 <sayimanoj@gmail.com>
2018-01-24 00:30:23 +0530

    Commit 100:
    Found a way to implement the widgets

--- app/src/main/java/com/hustler/quote/ui/Widgets/EditWidgetProvider2.java
+++ app/src/main/java/com/hustler/quote/ui/Widgets/EditWidgetProvider2.java
@@ -20,22 +20,8 @@ import com.hustler.quote.ui.apiRequestLauncher.Constants;
 public class EditWidgetProvider2 extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
-        }
+
 
     }
 
-    private void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
-        RemoteViews remoteView = new RemoteViews(context.getPackageName(), R.layout.quote_widget_configuration_layout);
-        Intent intent_edit = new Intent(context, EditorActivity.class);
-        Intent intent_home = new Intent(context, HomeActivity.class);
-        intent_edit.putExtra(Constants.INTENT_IS_FROM_EDIT_KEY, false);
-        PendingIntent pendingIntent_edit = PendingIntent.getActivity(context, 0, intent_edit, 0);
-        PendingIntent pendingIntent_home = PendingIntent.getActivity(context, 0, intent_home, 0);
-        remoteView.setOnClickPendingIntent(R.id.edit_widget, pendingIntent_edit);
-        remoteView.setOnClickPendingIntent(R.id.edit_widget_quotes, pendingIntent_home);
-
-        appWidgetManager.updateAppWidget(appWidgetId, remoteView);
-    }
 }
--- app/src/main/java/com/hustler/quote/ui/Widgets/QuoteWidgetConfigurationActivity.java
+++ app/src/main/java/com/hustler/quote/ui/Widgets/QuoteWidgetConfigurationActivity.java
@@ -1,17 +1,22 @@
 package com.hustler.quote.ui.Widgets;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RemoteViews;
 import android.widget.TextView;
 
 import com.hustler.quote.R;
 import com.hustler.quote.ui.activities.BaseActivity;
+import com.hustler.quote.ui.activities.EditorActivity;
+import com.hustler.quote.ui.apiRequestLauncher.Constants;
 
 import static android.appwidget.AppWidgetManager.INVALID_APPWIDGET_ID;
 
@@ -30,8 +35,9 @@ public class QuoteWidgetConfigurationActivity extends BaseActivity implements Vi
     private Button colorChanger;
     private Button SizeChanger;
     private Button addWidget;
-
+    RemoteViews remoteViews;
     int mAppWidgetId;
+    AppWidgetManager appWidgetManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,6 +45,8 @@ public class QuoteWidgetConfigurationActivity extends BaseActivity implements Vi
         setContentView(R.layout.quote_widget_configuration_layout);
         findViews();
         mAppWidgetId = INVALID_APPWIDGET_ID;
+        appWidgetManager = AppWidgetManager.getInstance(this);
+        remoteViews = new RemoteViews(this.getPackageName(), R.layout.quote_widget_layout);
         intialiseAppWidget();
 
 
@@ -79,14 +87,31 @@ public class QuoteWidgetConfigurationActivity extends BaseActivity implements Vi
             // Handle clicks for colorChanger
 
             quoteBody.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimaryDark));
+            remoteViews.setTextColor(R.id.quote_body, ContextCompat.getColor(getApplicationContext(), R.color.colorPrimaryDark));
 
         } else if (v == SizeChanger) {
             // Handle clicks for SizeChanger
-            quoteBody.setTextSize(10);
+            quoteBody.setTextSize(65);
+            remoteViews.setTextViewTextSize(R.id.quote_body, TypedValue.COMPLEX_UNIT_DIP, 65);
+//            remoteViews.setInt(R.id.root_widget,"setBackgroundResource",R.drawable.alphawhite);
+//            remoteViews.setInt(R.id.root_widget,"setBackgroundResource",R.drawable.alphawhite);
         } else if (v == addWidget) {
+//            LinearLayout linearLayout = (LinearLayout) findViewById(R.id.root_widget);
+//            AnimationDrawable animationDrawable = (AnimationDrawable) linearLayout.getBackground();
+//            animationDrawable.setEnterFadeDuration(2000);
+//            animationDrawable.setExitFadeDuration(4000);
+//            animationDrawable.start();
+            remoteViews.setInt(R.id.root_widget, "setBackGroundResource", R.drawable.animation_list);
+
             colorChanger.setVisibility(View.GONE);
             SizeChanger.setVisibility(View.GONE);
             addWidget.setVisibility(View.GONE);
+            Intent intent_edit = new Intent(this, EditorActivity.class);
+            intent_edit.putExtra(Constants.INTENT_IS_FROM_EDIT_KEY, false);
+            PendingIntent pendingIntent_edit = PendingIntent.getActivity(this, 0, intent_edit, 0);
+            remoteViews.setOnClickPendingIntent(R.id.root_widget, pendingIntent_edit);
+            appWidgetManager.updateAppWidget(mAppWidgetId, remoteViews);
+
             Intent resultValue = new Intent();
             resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
             setResult(RESULT_OK, resultValue);
